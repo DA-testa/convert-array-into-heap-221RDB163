032ef1992351663a7bd1053d690089aea77af097
221RDB163 <125826072+221RDB163@users.noreply.github.com>
2023-03-18 22:01:39 +0000

    Main

--- build_heap.py
+++ build_heap.py
@@ -2,69 +2,69 @@
 import math
 
 def heap_sort(data, i, swaps):
-    smallest_number = i
-    left_number = 2*i+1
-    right_number = 2*i+2
+	smallest_number = i
+	left_number = 2*i+1
+	right_number = 2*i+2
 
-    if(left_number < len(data) and data[left_number] < data[smallest_number]):
-        smallest_number = left_number
+	if(left_number < len(data) and data[left_number] < data[smallest_number]):
+		smallest_number = left_number
 
-    if(right_number < len(data) and data[right_number] < data[smallest_number]):
-        smallest_number = right_number
+	if(right_number < len(data) and data[right_number] < data[smallest_number]):
+		smallest_number = right_number
 
-    if(smallest_number != i):
-        (data[i], data[smallest_number]) = (data[smallest_number], data[i])
-        swaps.append([i, smallest_number])
+	if(smallest_number != i):
+		(data[i], data[smallest_number]) = (data[smallest_number], data[i])
+		swaps.append([i, smallest_number])
 
-        heap_sort(data, smallest_number, swaps)
-    
-    return swaps
+		heap_sort(data, smallest_number, swaps)
+	
+	return swaps
 
 def build_heap(data):
-    swaps = []
+	swaps = []
 
-    for i in range(math.floor(len(data)/2), -1, -1):
-        heap_sort(data, i, swaps)
+	for i in range(math.floor(len(data)/2), -1, -1):
+		heap_sort(data, i, swaps)
 
-    for i in range(len(data), 0, -1):
-        heap_sort(data, i, swaps)
+	for i in range(len(data), 0, -1):
+		heap_sort(data, i, swaps)
 
-    return swaps
+	return swaps
 
 def main():
-    
-    option = input()
-    data = []
+	
+	option = input("Enter input type: ")
+	data = []
 
-    if option in "F":
-        # input from file
-        try:
-            filePath = input()
-            
-            with open("tests/" + str(filePath)) as file:
-                n = int(file.readline())
-                data = list(map(int, file.readline().split()))
-                file.close()
-        except FileNotFoundError:
-            return
-    elif option in "I":
-        # input from keyboard
-        try:
-            n = int(input())
-            data = list(map(int, input().split()))
-        except ValueError:
-            return
-    else:
-        return
+	if option in "F":
+		# input from file
+		try:
+			filePath = input("Input file path: ")
+			
+			with open("tests/" + str(filePath)) as file:
+				n = int(file.readline())
+				data = list(map(int, file.readline().split()))
+				file.close()
+		except FileNotFoundError:
+			return
+	elif option in "I":
+		# input from keyboard
+		try:
+			n = int(input())
+			data = list(map(int, input().split()))
+		except ValueError:
+			return
+	else:
+		return
 
-    assert len(data) == n
+	assert len(data) == n
 
-    swaps = build_heap(data)
+	swaps = build_heap(data)
 
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
+	print(len(swaps))
+	for i, j in swaps:
+		print(i, j)
 
 
 if __name__ == "__main__":
-    main()
+	main()
